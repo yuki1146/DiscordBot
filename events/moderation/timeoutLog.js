7e0de27e06e172cd3d55f10a2b19aae47352f087
ゆき <2023dai2andosatoshi6@gmail.com>
2024-10-14 19:18:24 +0900

    Create timeoutLog.js

---
+++ events/moderation/timeoutLog.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+const { channelId } = require('../../config.json');
+
+module.exports = {
+    name: 'guildMemberUpdate',
+    async execute(oldMember, newMember) {
+        if (oldMember.communicationDisabledUntilTimestamp !== newMember.communicationDisabledUntilTimestamp) {
+            const logChannel = await newMember.guild.channels.fetch(channelId);
+            const embed = new EmbedBuilder()
+                .setTitle('ユーザーがタイムアウトされました')
+                .setColor(0x0000ff)
+                .addFields(
+                    { name: 'ユーザー名', value: newMember.user.tag },
+                    { name: 'ユーザーID', value: newMember.user.id },
+                    { name: 'タイムアウトの終了', value: newMember.communicationDisabledUntil ? newMember.communicationDisabledUntil.toUTCString() : 'なし' }
+                )
+                .setTimestamp();
+
+            if (logChannel) {
+                logChannel.send({ embeds: [embed] });
+            }
+        }
+    },
+};
