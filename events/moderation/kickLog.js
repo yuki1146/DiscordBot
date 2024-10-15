ebec1740bfec9a0fc7f0520a62bc56d44f2d83f2
ゆき <2023dai2andosatoshi6@gmail.com>
2024-10-15 17:25:05 +0900

    Update kickLog.js

--- events/moderation/kickLog.js
+++ events/moderation/kickLog.js
@@ -2,20 +2,23 @@ const { EmbedBuilder } = require('discord.js');
 const { channelId } = require('../../config.json');
 
 module.exports = {
-    name: 'guildMemberRemove',
-    async execute(member) {
-        const logChannel = await member.guild.channels.fetch(channelId);
-        const embed = new EmbedBuilder()
-            .setTitle('ユーザーがKickされました')
-            .setColor(0xffa500)
-            .addFields(
-                { name: 'ユーザー名', value: member.user.tag },
-                { name: 'ユーザーID', value: member.user.id }
-            )
-            .setTimestamp();
+    name: 'messageUpdate',
+    async execute(oldMessage, newMessage) {
+        if (oldMessage.content !== newMessage.content) {
+            const logChannel = await oldMessage.guild.channels.fetch(channelId);
+            const embed = new EmbedBuilder()
+                .setTitle('メッセージが編集されました。')
+                .setColor(0xffff00)
+                .addFields(
+                    { name: 'メッセージ送信者', value: oldMessage.author.tag || '不明' },
+                    { name: '編集前のメッセージ', value: oldMessage.content || '不明' },
+                    { name: '編集後のメッセージ', value: newMessage.content || '不明' }
+                )
+                .setTimestamp();
 
-        if (logChannel) {
-            logChannel.send({ embeds: [embed] });
+            if (logChannel) {
+                logChannel.send({ embeds: [embed] });
+            }
         }
     },
 };
